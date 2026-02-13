1dfb70ad61a6675442a1c6e875d5f93426696f12
Arham <arhamali63@gmail.com>
2026-02-13 16:47:11 +0500

    props and conditional rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import './App.css'
+import Input from './components/input'
 
 function App() { //component
 
+  const getAlert = () => {
+    alert("Hello world")
+  }
+
   return (
-    <h1>Hello world</h1>
+    <>
+      <h1>Hello world</h1>
+      <Input type='text' eyeIcon={false} />
+      <Input type='number' eyeIcon={false} />
+      <Input type='password' eyeIcon={true} userFunction={getAlert} />
+      <Input type='email' eyeIcon={false} />
+    </>
   )
 }
 
